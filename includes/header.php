d46e12ed457fa599c047cae49addaaba1a83d6c9
Zwivhuya Davhana <113597052+Zwivhuya6132@users.noreply.github.com>
2023-07-26 09:53:12 +0200

    converting to php

---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ulink</title>
+    <link rel="website icon" type="png" href="images/uj.png">
+    <link rel="stylesheet" href="all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Link.css">
+</head>
+<body>
+    <header class="head">
+
+        <div class="head-left">
+            <img src="images/uj.png" alt="uj logo" class="logo">
+            
+            <ul class="nav-links">
+                <li><i class="fa-solid fa-power-off"></i>&nbsp;&nbsp;&nbsp;Log-out</li>
+                <li><i class="fa-solid fa-question"></i>&nbsp;&nbsp;&nbsp;Help</li>
+                <li><i class="fa-regular fa-bell"></i>&nbsp;&nbsp;&nbsp;Notification</li>
+            </ul>
+        </div>
+
+        <div class="head-right">
+
+            <small>
+                <p>Welcome Mr Z Davhana</p>
+                <p> zwivhuyadavhana6132@gmail.com </p>
+            </small>
+
+            <small class="big-ulink">
+                 <h1>Ulink</h1>
+            </small>
+
+        </div>
+        
+    </header>
